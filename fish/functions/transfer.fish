7d839d12a934733ea4cde2cbb2c2821fb97431d0
evicinelli <emanuele.vicinelli@gmail.com>
2017-06-26 23:35:49 +0200

    Spazio in i3/config, transfer.fish ora mostra percentuale completamento

--- fish/functions/transfer.fish
+++ fish/functions/transfer.fish
@@ -1,3 +1,3 @@
 function transfer
-	curl -H "Max-Downloads: 3" -H "Max-Days: 1" --progress-bar --upload-file $argv https://transfer.sh/$argv
+	touch tmpCurlStatus; curl -H "Max-Downloads: 3" -H "Max-Days: 1" --progress-bar --upload-file $argv https://transfer.sh/$argv >> tmpCurlStatus; cat tmpCurlStatus; rm -f tmpCurlStatus
 end
