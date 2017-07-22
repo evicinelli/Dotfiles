a8e62db761a2384ad4a6ccc4e2f79a7304ab4524
evicinelli <emanuele.vicinelli@gmail.com>
2017-07-22 18:34:50 +0200

    Cleanup

--- fish/functions/transfer.fish
+++ fish/functions/transfer.fish
@@ -1,3 +1,3 @@
 function transfer
-	touch tmpCurlStatus; curl -H "Max-Downloads: 3" -H "Max-Days: 1" --progress-bar --upload-file $argv https://transfer.sh/$argv >> tmpCurlStatus; cat tmpCurlStatus | qrencode -o qr.png; open qr.png; cat tmpCurlStatus; rm -f tmpCurlStatus; rm -f qr.png
+	touch tmpCurlStatus; curl -H "Max-Downloads: 3" -H "Max-Days: 1" --progress-bar --upload-file $argv https://transfer.sh/$argv >> tmpCurlStatus; cat tmpCurlStatus | qrencode -o qr.png; feh -xZ qr.png; cat tmpCurlStatus; rm -f tmpCurlStatus; rm -f qr.png
 end
