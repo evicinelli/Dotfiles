fb63981b1d9a7eb31e7692aaf0b8e5871eda7c51
evicinelli <emanuele.vicinelli@gmail.com>
2017-09-05 09:43:26 +0200

    riportiamo un po' di sanità

--- dmenu/clipboard.sh
+++
@@ -1,3 +0,0 @@
-# clipboard
-# rofi -modi "clipboard:greenclip print" -show clipboard
-bash /home/(whoami)/Dotfiles/dmenu/clipmenu/clipmenu -i -p "Clipboard" -fn "Inconsolata-13"
--- dmenu/web.sh
+++
@@ -1,3 +0,0 @@
-# web
-google-chrome https://duckduckgo.com/?q="$(dmenu -p "Web" -fn "Inconsolata-13")"
-i3 [class = "Google-chrome"] focus
