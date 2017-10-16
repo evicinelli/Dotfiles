f3385f8cbf0a6bf02fd3937e674817e39d5ef01e
evicinelli <emanuele.vicinelli@gmail.com>
2017-10-16 23:06:22 +0200

    Prove per install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install roba essential  
-apt install git wget i3 i3lock vim gvim lxappearance arandr zip unzip xfce4-power-manager xfce4-volumed compton redshift-gtk dmenu dunst
+apt install git wget i3 i3lock vim gvim lxappearance arandr zip unzip xfce4-power-manager xfce4-volumed compton redshift-gtk dunst
 
 # Install more stuff
 apt install fonts-inconsolata
@@ -15,10 +15,13 @@ apt install fonts-inconsolata
 # Cloning Dotfiles
 cd $HOME
 git clone https://github.com/evicinelli/Dotfiles.git
-ln -sf i3 $HOME/.config/i3
-ln -sf fish $HOME/.config/fish
-ln -sf rofi $HOME/.config/rofi
-ln -sf terminator $HOME/.config/terminator
-ln -sf sublime-text $HOME/.config/sublime-text-3
-ln -sf compton.conf $HOME/.compton.conf
-ln -sf dunstrc /etc/xdg/dunst/dunstrc
+ln -sfn i3 $HOME/.config/i3
+ln -sfn fish $HOME/.config/fish
+ln -sfn rofi $HOME/.config/rofi
+ln -sfn terminator $HOME/.config/terminator
+ln -sfn sublime-text $HOME/.config/sublime-text-3
+ln -sfn compton.conf $HOME/.compton.conf
+ln -sfn vim/gvim $HOME/.gvim
+ln -sfn vim/vim $HOME/.vim
+ln -sf vim/gvimrc $HOME/.gvimrc
+sudo ln -sf dunstrc /etc/xdg/dunst/dunstrc
--- rofi/file.sh
+++ rofi/file.sh
@@ -1,2 +1,2 @@
 # find files
-xdg-open "$(find /tmp /home/$(whoami) /media/$(whoami) -not -path "/home/$(whoami)/.*" -not -path "/home/$(whoami)/Scaricati/App*" | rofi -levenshtein-sort -dmenu -i -p "File ")"
+xdg-open "$(find /home/$(whoami) /tmp /media/$(whoami) -not -path "/home/$(whoami)/.*" -not -path "/home/$(whoami)/Scaricati/App*" | rofi -levenshtein-sort -dmenu -i -p "File ")"
