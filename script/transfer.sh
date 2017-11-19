bcffc8e6830329b78651d807e3a45c8c24e44356
Emanuele Vicinelli <emanuele.vicinelli@gmail.com>
2017-11-19 18:46:36 +0100

    Transfer.sh funziona con ctrl-v

--- script/transfer.sh
+++ script/transfer.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
-if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
-tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl -H "Max-Downloads: 3" -H "Max-Days: 2"-progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile | xclip -selection primary; rm -f $tmpfile; 
+if [ $# -eq 0 ]; 
+    then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; 
+    return 1; 
+fi
+
+tmpfile=$( mktemp -t transferXXX );
+if tty -s; then 
+    basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); 
+    curl -H "Max-Downloads: 3" -H "Max-Days: 2" --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; 
+else curl -H "Max-Downloads: 3" -H "Max-Days: 2" --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; 
+fi; 
+cat $tmpfile | xclip -selection c;
+cat $tmpfile; 
+rm -f $tmpfile; 
