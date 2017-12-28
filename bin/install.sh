1ffb5be9f0f42f32d74e7486e80ba7cc2b544e3c
Emanuele Vicinelli <emanuele.vicinelli@gmail.com>
2017-12-28 23:01:51 +0100

    remindme bash script

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 ## Script di post installazione e configurazione 
+
 # Configuriamo git
 git config --global user.email "emanuele.vicinelli@gmail.com"
 git config --global user.name "Emanuele Vicinelli"
@@ -14,30 +15,15 @@ sudo dpkg -i playerctl5.deb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-# Sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-
 # Rimuovo le cose non necessarie
 sudo apt remove gedit
 
 # Installo le cose
 sudo apt update
-sudo apt install i3 i3blocks rofi vim vim-gtk3 terminator\ # fish 
-fonts-inconsolata compton arandr dunst \
-spotify-client sublime-text lxappearance xfce4-power-manager pavucontrol build-essential curl\
-redshift-gtk vlc zip ufw tree xbacklight pass xclip rsync owncloud-client gnome-sushi gufw ranger
-
-# Oh my fish
-# cd $HOME/Scaricati
-# curl -L https://get.oh-my.fish > install
-# fish install --path=~/.local/share/omf --config=~/.config/omf
+sudo apt install i3 i3blocks rofi vim vim-gtk3 terminator fonts-inconsolata compton arandr dunst spotify-client lxappearance xfce4-power-manager pavucontrol build-essential curl redshift-gtk vlc zip ufw tree xbacklight pass xclip rsync owncloud-client gnome-sushi gufw ranger at apt-transport-http libnotify-bin
 
 # Vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Aggiorno e pulisco tutto
 sudo apt upgrade
@@ -50,13 +36,11 @@ sudo apt autoclean
 mkdir $HOME/.config/dunst
 ln -sf $HOME/Dotfiles/i3 $HOME/.config/i3
 ln -sf $HOME/Dotfiles/terminator $HOME/.config/terminator
-# ln -sf $HOME/Dotfiles/fish $HOME/.config/fish
 ln -sf $HOME/Dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/Dotfiles/rofi $HOME/.config/rofi
-ln -sf $HOME/Dotfiles/sublime-text $HOME/.config/sublime-text-3
 ln -sf $HOME/Dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf $HOME/Dotfiles/compton.conf $HOME/.compton.conf
 ln -sf $HOME/Dotfiles/mimeapps.cache $HOME/.local/share/applications/mimeinfo.cache
 sudo ln -sf $HOME/Dotfiles/dunstrc $HOME/.config/dunst/dunstrc
 
-echo "Fatto"
+echo "Fatto, fai meglio a controllare però"
---
+++ bin/remindme.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [[ $# -gt 0 ]]; then
+#    if [[ $1 -ne "^[0-9][0-9]:[0-9][0-9]" ]]; then
+#	echo "Ancora non ci siamo"
+#    else
+	echo "Ok, alle $1 ti ricorderò di $2"
+	at "$1" <<< "notify-send --urgency=critical \"REMINDME:\" \"$2\""
+#    fi
+else
+    echo "Nulla di fatto"
+fi
