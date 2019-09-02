ebcaeed5d7ee86de2e831be435c68cdf5eb58457
Emanuele Vicinelli <emanuele.vicinelli@gmail.com>
2019-09-02 15:59:56 +0200

    argos

--- argos/.config/argos/2battery.10s+.sh
+++ argos/.config/argos/2battery.10s+.sh
@@ -1,1 +1,19 @@
-/home/vic/Dotfiles/bin/.config/argos/2battery.10s+.sh
+#! /usr/bin/env bash
+
+BATTERY_FILE=/sys/class/power_supply/BAT0/uevent
+
+# Get battery info
+source $BATTERY_FILE
+
+if [[ $POWER_SUPPLY_CAPACITY -gt 50 ]]; then 
+    COLOR=lightgreen
+elif [[ $POWER_SUPPLY_CAPACITY -gt 30 ]]; then
+    COLOR=yellow
+else
+    COLOR=red
+fi
+if [[ $POWER_SUPPLY_STATUS == "Charging" ]]; then COLOR=cyan; fi
+
+echo "⚡️ $POWER_SUPPLY_CAPACITY % | color=$COLOR"
+echo "---"
+acpi -V | grep -v "no state information available" | grep -v "Cooling"
--- bin/tmux-status.sh
+++ bin/tmux-status.sh
@@ -15,6 +15,6 @@ function get_todo_from_file() {
     echo "#[fg=$color bg=colour0] $symbol#[default]"
 }
 
-echo "BAT:$(cat /sys/class/power_supply/BAT0/capacity)% ∙ $(nm-online -t 0 && echo 'ONLINE' || echo 'OFFLINE') ∙ VOL:$(amixer | grep Front | grep Playback | cut -d'%' -f1 | cut -d '[' -f2 | head -n2 | tail -n1)% #[bg=colour8 fg=colour15] $(date +%H:%M\ %a\ %d ) $(get_todo_from_file)"
+echo "BAT:$(cat /sys/class/power_supply/BAT0/capacity)% ∙ $(nm-online -t 0 && echo 'ONLINE' || echo 'OFFLINE') ∙ VOL:$(amixer | grep Front | grep Playback | cut -d'%' -f1 | cut -d '[' -f2 | head -n2 | tail -n1)% #[bg=colour8 fg=colour0] $(date +%H:%M\ %a\ %d ) $(get_todo_from_file)"
 
 # ∙ $([[ `playerctl status` -ne 'No players found' ]] && echo `playerctl artist` - `playerctl title` || echo "NP")"
